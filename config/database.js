6dce49755952131c039403de912a99b96a5c5869
Gbemi <kadri.oluwagbemi@gmail.com>
2024-12-17 02:22:34 +0200

    Update database config

--- config/database.js
+++ config/database.js
@@ -1,13 +1,6 @@
 import { MongoClient, ServerApiVersion } from 'mongodb'
 
-const username = encodeURIComponent(process?.env?.DB_USERNAME || '')
-const password = encodeURIComponent(process?.env?.DB_PASSWORD || '')
-const host = encodeURIComponent(process?.env?.DB_HOST || '')
-const appName = encodeURIComponent(process?.env?.DB_APP_NAME || '')
-
-const uri = `mongodb+srv://${username}:${password}@${host}/?retryWrites=true&w=majority&appName=${appName}`
-
-const client = new MongoClient(uri, {
+const client = new MongoClient(process.env.MONGODB_URI, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
