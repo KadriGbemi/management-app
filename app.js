6e0643865388b7a9cac6cfc52b99b133b34058d7
Gbemi <kadri.oluwagbemi@gmail.com>
2024-12-18 18:43:51 +0200

    Cors setup for minimal security

--- app.js
+++ app.js
@@ -1,46 +1,62 @@
-import createError from 'http-errors';
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
+import createError from 'http-errors'
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import cookieParser from 'cookie-parser'
+import logger from 'morgan'
+import cors from 'cors'
 
-import indexRouter from './routes/index.js';
-import employeeRouter from './routes/employee.js';
+import indexRouter from './routes/index.js'
+import employeeRouter from './routes/employee.js'
 
-import 'dotenv/config';
+import 'dotenv/config'
 
-const app = express();
+const app = express()
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || ['http://localhost:5050'].includes(origin)) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  methods: ['GET', 'POST', 'OPTIONS', 'PUT'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+app.use(cors(corsOptions))
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter);
-app.use('/api', employeeRouter);
+app.use('/', indexRouter)
+app.use('/api', employeeRouter)
 
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use(function (req, res, next) {
+  next(createError(404))
+})
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  res.status(err.status || 500)
+  res.render('error')
+})
 
-export default app;
+export default app
--- routes/employee.js
+++ routes/employee.js
@@ -1,6 +1,21 @@
 import express from 'express'
+import cors from 'cors'
 import employeeController from '../controller/employee.controller.js'
 import influencerController from '../controller/influencer.controller.js'
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (['http://localhost:5050'].indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  methods: ['GET', 'POST', 'OPTIONS', 'PUT'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
 const router = express.Router()
 
@@ -9,7 +24,7 @@ router.get('/', function (req, res, next) {
 })
 
 /* GET employees listing. */
-router.get('/employees', employeeController.getEmployees)
+router.get('/employees',  employeeController.getEmployees)
 
 router.get('/employees/:employeeId', employeeController.getEmployeeById)
 
